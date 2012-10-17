3b47dbf2831bd8ec068b3c3d17e05ef86ef223ed
Raymond Wong <rayman_407@yahoo.com>
2012-10-17 11:47:28 -0700

    Created friend-app-match rails web app.

---
+++ friend-app-matcher/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FriendAppMatcher::Application.config.secret_token = 'dd988b13ee1f534cb6ad4d008db0ea7c04d553cec92cb185b9a2a406ad9f0a6418ab8f50e0cd79d1402bbc46aa8bc14683d2b32c821b2668a8350b383d88a3df'
