661422c100f4484faf0f01d8cf884bdf43714862
Raymond Wong <rayman_407@yahoo.com>
2012-10-21 14:08:07 -0700

    Modified many-to-many relationships of friends, although uncertain if done correctly.  Updated seed script to use fewer users.

--- friend-app-matcher/app/models/friend.rb
+++ friend-app-matcher/app/models/friend.rb
@@ -1,5 +1,7 @@
 class Friend < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :user, :class_name => :User
+  belongs_to :friend, :class_name => :User
+
   attr_accessible :user_id, :friend_id
   validates_uniqueness_of :user_id, :scope => :friend_id
   validate :user_is_not_friend_to_self
