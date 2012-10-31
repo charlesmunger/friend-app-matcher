36353cd439d5a1d3e6f10508c179f21d02fc9221
Raymond Wong <rayman_407@yahoo.com>
2012-10-30 22:11:25 -0700

    Started a general layout for all pages once logged in to the web service. Done so far: 1) Header with the ability to logout
    TODO:
    Side panel and main content of each page (tables)

---
+++ friend-app-matcher/app/assets/javascripts/applayout.js
@@ -0,0 +1,6 @@
+$(function() {
+  // Toggle display off user options when clicking on name
+  $('.app-header-content .name-display').on('click', function() {
+    $('.app-header-content .user-options-sub').toggle();
+  });
+});
