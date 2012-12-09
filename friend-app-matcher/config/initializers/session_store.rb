7002fdff233f7d8a707c0d721c33e137183217c7
Guillaume <guillaume.delente@gmail.com>
2012-12-09 12:36:57 -0800

    Enable session cache

--- friend-app-matcher/config/initializers/session_store.rb
+++ friend-app-matcher/config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 FriendAppMatcher::Application.config.session_store :cookie_store, key: '_friend-app-matcher_session'
-
+FriendAppMatcher::Application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 20.minutes
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
